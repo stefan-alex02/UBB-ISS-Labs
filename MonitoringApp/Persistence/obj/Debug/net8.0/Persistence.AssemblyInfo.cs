22f070cd249077b7a9747208deddbb1306642771
stefan-alex02 <43996943+stefan-alex02@users.noreply.github.com>
2024-05-13 16:48:32 +0300

    Small changes

--- MonitoringApp/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ MonitoringApp/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458f4f74dd201252f88efd2f0e836c92a2b93cb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85e777f3ef852875e648361ced42759273182a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
